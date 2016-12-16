6d2337f7561c2cfac307f8e3b7c416e498b17884
ralf <ralf000@yandex.ru>
2016-12-16 18:33:26 +0300

    рассмотрены основы маршрутизации laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,54 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Определяем имя роута для использования в дальнейшем
+ * имя можно использовать далее в других роутах
+ * также определяем параметр id
+ * Выведет на странице "http://laravel/page/id?10"
+ */
+/*
+Route::get('/page/{id}', function ($id) {
+    echo route('pagename', ['id', $id]) . '<br>';
+})->name('pagename');
+*/
+
+/**
+ * добавляем редирект по заранее определенному имени роута
+ * также определяем параметр id для передачи роуту
+ */
+/*
+Route::get('/otherpage', function () {
+    return redirect()->route('pagename', ['id' => 25]);
+});
+*/
+
+/**
+ * Пример передачи параметров
+ * /page/hello/10
+ */
+/*
+Route::get('/page/{cat}/{id}', function ($cat, $id) {
+    echo $cat, $id;
+})->where([
+    'id' => '[0-9]+',
+    'cat' => '[a-zA-Z]+'
+]);
+*/
+
+/**
+ * Пример группировки маршрутов
+ * Теперь машруты ниже доступны по адресу /admin/page/create
+ */
+/*
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/page/create', function () {
+        echo 'page/create';
+    });
+
+    Route::get('/page/edit', function () {
+        echo 'page/edit';
+    });
+});
+*/
