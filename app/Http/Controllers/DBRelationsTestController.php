3bebb00afb9475e6d5241cf79116a83c8a335f9f
ralf <ralf000@yandex.ru>
2017-01-13 10:08:27 +0300

    Жадная и ленивая загрузка связанные данных из таблиц Метод has

--- app/Http/Controllers/DBRelationsTestController.php
+++ app/Http/Controllers/DBRelationsTestController.php
@@ -27,6 +27,44 @@ class DBRelationsTestController extends Controller
 
 //        dd($role->users);
 
+        /**
+         * Пример ленивой загрузки
+         * Данные о пользователе подгружаются при каждой итерации
+         * как следствие - куча запросов к бд
+         */
+//        $pages = Page::all();
+//        foreach ($pages as $page) {
+//            echo $page->user->name;
+//        }
+
+        /**
+         * Жадная загрузка позволяет уменьшить количество запросов к бд
+         * связанные с моделью Page данные подгружаются сразу
+         * в методе with указывается имя динамического свойства
+         */
+//        $pages = Page::with('user')->get();
+//        foreach ($pages as $page) {
+//            echo $page->user->name;
+//        }
+
+        /**
+         * в случае когда данные уже выбраны методом all()
+         * и необходимо подгрузить сразу все связанные данные из другой таблицы
+         */
+//        $pages = Page::all();
+//        //какой-то код
+//        $pages->load('user');
+
+        /**
+         * Подгрузить все связанные данные из двух таблиц
+         */
+//        $users = User::with(['pages', 'roles'])->get();
+
+        /**
+         * Выбираем пользователей с тремя или более страницами
+         */
+//        $users = User::has('pages', '>=', 3)->get();
+
 
         return (view()->exists('relations'))
             ? view('relations', ['user' => $user, 'page' => $page])
