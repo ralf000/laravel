916f335fc6671f8115cf94b05df538ad396a8625
ralf <ralf000@yandex.ru>
2017-01-31 18:28:57 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,77 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+
+    Route::match(
+        ['get', 'post'],
+        '/',
+        ['uses' => 'IndexController@execute', 'as' => 'home']);
+    Route::get('/page/{alias}', ['uses' => 'PageController@execute', 'as' => 'page']);
+
+    Route::auth();
+
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+
+    // /admin
+    Route::get('/', function () {
+    });
+
+    Route::group(['prefix' => 'pages'], function () {
+
+        // /admin/pages
+        Route::get('/', ['uses' => 'PagesController@execute', 'as' => 'pages']);
+
+        // /admin/pages/add
+        Route::match(
+            ['get', 'post'],
+            '/add',
+            ['uses' => 'PagesAddController@execute', 'as' => 'pageAdd']);
+        // /admin/pages/edit/3
+        Route::match(
+            ['get', 'post', 'delete'],
+            '/edit/{page}',
+            ['uses' => 'PagesEditController@execute', 'as' => 'pageEdit']);
+
+    });
+
+    Route::group(['prefix' => 'portfolio'], function () {
+
+        // /admin/portfolio
+        Route::get('/', ['uses' => 'PortfolioController@execute', 'as' => 'portfolio']);
+
+        // /admin/portfolio/add
+        Route::match(
+            ['get', 'post'],
+            '/add',
+            ['uses' => 'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+        // /admin/portfolio/edit/3
+        Route::match(
+            ['get', 'post', 'delete'],
+            '/edit/{page}',
+            ['uses' => 'PortfolioEditController@execute', 'as' => 'portfolioEdit']);
+
+    });
+
+    Route::group(['prefix' => 'services'], function () {
+
+        // /admin/services
+        Route::get('/', ['uses' => 'ServiceController@execute', 'as' => 'services']);
+
+        // /admin/services/add
+        Route::match(
+            ['get', 'post'],
+            '/add',
+            ['uses' => 'ServiceAddController@execute', 'as' => 'serviceAdd']);
+        // /admin/services/edit/3
+        Route::match(
+            ['get', 'post', 'delete'],
+            '/edit/{page}',
+            ['uses' => 'ServiceEditController@execute', 'as' => 'serviceEdit']
+        );
+
+    });
+    
 });
