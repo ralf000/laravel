ca201b6afb49a2f6aad449214f7e7bc5f36e34b8
ralf <ralf000@yandex.ru>
2017-02-02 12:26:46 +0300

    +models +split layout into parts
    В основном шаблоне определяем места где будет выводится содержимое частей шаблона через yield
    Во вьюхе index наследуем основной шаблон и при помощи сексий выводим содержимое частей шаблона (через include) + контент

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Employee;
+use App\Page;
+use App\Portfolio;
+use App\Service;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
-        return view('layouts.site');
+        $pages = Page::all();
+        $portfolio = Portfolio::get(['name', 'image']);
+        $services = Service::where('id', '<', 20)->get();
+        $employee = Employee::take(3)->get();
+
+
+        return view('site.index');
     }
 }
