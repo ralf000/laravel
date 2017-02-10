e2d35ac8336c7b118122cba825fdd04a3b3750c0
ralf <ralf000@yandex.ru>
2017-02-10 18:31:16 +0300

    обработка формы редактирования материала

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -9,8 +9,39 @@ class PagesEditController extends Controller
 {
     public function execute(Page $page, Request $request)
     {
-        if ($request->isMethod('delete')){
+        if ($request->isMethod('post')) {
+            $input = $request->except('_token');
 
+            /**
+             * для alias исключаем из поиска текущую запись
+             */
+            $validator = \Validator::make($input, [
+                'title' => 'required|max:255',
+                'alias' => 'required|max:255|unique:pages,alias,' . $input['id'],
+                'text' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return redirect()
+                    ->route('pageEdit', ['page' => $input['id']])
+                    ->withErrors($validator);
+            }
+
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $originalName = $file->getClientOriginalName();
+                $file->storeAs('img', $originalName);
+                $input['image'] = $originalName;
+            } else {
+                $input['image'] = $input['old_image'];
+            }
+
+            unset($input['old_image']);
+
+            $page->fill($input);
+
+            if ($page->update()){
+                return redirect('admin')->with('status', 'Страница обновлена');
+            }
         }
         /**
          * если в текущем маршруте есть id
@@ -22,7 +53,7 @@ class PagesEditController extends Controller
 
         $old = $page->toArray();
 
-        if (view()->exists('admin.pages-edit')){
+        if (view()->exists('admin.pages-edit')) {
             $data = [
                 'title' => 'Редактирование страницы ' . $old['title'],
                 'data' => $old
