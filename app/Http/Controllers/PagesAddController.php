094c25d85ab19bdf61320c5a6b778fb29f57a747
ralf <ralf000@yandex.ru>
2017-02-10 17:08:13 +0300

    сохранение информации из формы в базе данных сохранение файлов (изображений) изменение пути сохранения изображений перевод сообщений об ошибках

--- app/Http/Controllers/PagesAddController.php
+++ app/Http/Controllers/PagesAddController.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Page;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class PagesAddController extends Controller
 {
     public function execute(Request $request)
     {
         if ($request->isMethod('post')) {
+            $input = $request->except('_token');
+
+            $validator = \Validator::make($input, [
+                'title' => 'required|max:255',
+                'alias' => 'required|unique:pages|max:100',
+                'text' => 'required'
+            ]);
+            if ($validator->fails()) {
+                return redirect()
+                    ->route('pageAdd')
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+
+            /**
+             * @var $file UploadedFile
+             */
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $input['image'] = $file->getClientOriginalName();
+                $file->storeAs('img', $input['image']);
+            }
+
+            $page = new Page();
+            /**
+             * разрешить заполнение полей модели можно:
+             * в самой модели указать свойство $fillable
+             */
+            $page->fill($input);
+
+            if ($page->save())
+                return redirect('admin')->with('status', 'Страница добавлена');
 
         }
 
